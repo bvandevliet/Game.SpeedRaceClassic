cd49e403c22ec5adcc523d5dfe6c1b85a1a33ef7
Bob Vandevliet <info@bvandevliet.nl>
2023-12-29 20:57:36 +0100

    Code cleanup and fixes.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 /* eslint-disable func-style */
 /* eslint-disable no-implicit-globals */
 
+/**
+ * @type {HTMLCanvasElement}
+ */
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 const player = {
-  x: canvas.width / 2 - 20,
-  y: canvas.height - 60,
   width: 40,
   height: 60,
+  x: canvas.width / 2 - 20,
+  y: canvas.height - 180,
   color: 'blue',
   speed: 5,
   movingLeft: false,
@@ -16,55 +19,41 @@ const player = {
 };
 
 const enemies = [];
+
 let score = 0;
 let gameOver = false;
 
-document.addEventListener('keydown', handleKeyDown);
-document.addEventListener('keyup', handleKeyUp);
-
-function handleKeyDown (event)
+document.addEventListener('keydown', e =>
 {
-  if (event.key === 'ArrowLeft')
+  if (e.key === 'ArrowLeft')
   {
     player.movingLeft = true;
   }
-  else if (event.key === 'ArrowRight')
+  else if (e.key === 'ArrowRight')
   {
     player.movingRight = true;
   }
-}
+});
 
-function handleKeyUp (event)
+document.addEventListener('keyup', e =>
 {
-  if (event.key === 'ArrowLeft')
+  if (e.key === 'ArrowLeft')
   {
     player.movingLeft = false;
   }
-  else if (event.key === 'ArrowRight')
+  else if (e.key === 'ArrowRight')
   {
     player.movingRight = false;
   }
-}
-
-function drawPlayer ()
-{
-  ctx.fillStyle = player.color;
-  ctx.fillRect(player.x, player.y, player.width, player.height);
-}
-
-function drawEnemy (enemy)
-{
-  ctx.fillStyle = 'red';
-  ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
-}
+});
 
 function movePlayer ()
 {
-  if (player.movingLeft && player.x > 0)
+  if (player.movingLeft) // && player.x > 0)
   {
     player.x -= player.speed;
   }
-  else if (player.movingRight && player.x < canvas.width - player.width)
+  else if (player.movingRight) // && player.x < canvas.width - player.width)
   {
     player.x += player.speed;
   }
@@ -72,22 +61,20 @@ function movePlayer ()
 
 function moveEnemies ()
 {
-  for (let i = 0; i < enemies.length; i++)
-  {
-    enemies[i].y += 2;
-
-    // Move left and right at a static speed
-    enemies[i].x += enemies[i].speed;
-
-    // Reverse direction when hitting the sidewalls
-    if (enemies[i].x < 0 || enemies[i].x + enemies[i].width > canvas.width)
-    {
-      enemies[i].speed *= -1;
-    }
-  }
-
   enemies.forEach((enemy, index) =>
   {
+    // Move the enemy down.
+    enemy.y += 2;
+
+    // Move the enemy sideways.
+    enemy.x += enemy.speed;
+
+    // Reverse direction when hitting the sidewalls.
+    if (enemy.x < 0 || enemy.x + enemy.width > canvas.width)
+    {
+      enemy.speed *= -1;
+    }
+
     if (enemy.y > canvas.height)
     {
       enemies.splice(index, 1);
@@ -98,23 +85,14 @@ function moveEnemies ()
 
 function checkCollisions ()
 {
-  enemies.forEach(enemy =>
-  {
-    if (
-      player.x < enemy.x + enemy.width
-            && player.x + player.width > enemy.x
-            && player.y < enemy.y + enemy.height
-            && player.y + player.height > enemy.y
-    )
-    {
-      gameOver = true;
-    }
-
-    if (player.x < 0 || player.x + player.width > canvas.width)
-    {
-      gameOver = true;
-    }
-  });
+  gameOver = enemies.some(enemy =>
+    // Game over if the player hits an enemy.
+    (player.x < enemy.x + enemy.width
+      && player.x + player.width > enemy.x
+      && player.y < enemy.y + enemy.height
+      && player.y + player.height > enemy.y)
+    // Game over if the player hits a sidewall.
+    || (player.x < 0 || player.x + player.width > canvas.width));
 }
 
 function update ()
@@ -129,16 +107,26 @@ function update ()
 
 function draw ()
 {
+  // Clear the canvas.
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawPlayer();
+  // Draw the player.
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.width, player.height);
 
-  enemies.forEach(drawEnemy);
+  // Draw the enemies.
+  enemies.forEach(enemy =>
+  {
+    ctx.fillStyle = enemy.color;
+    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+  });
 
+  // Draw the score.
   ctx.fillStyle = 'black';
   ctx.font = '20px Arial';
   ctx.fillText(`Score: ${score}`, 10, 30);
 
+  // Draw the game over message.
   if (gameOver)
   {
     ctx.fillText('Game Over', canvas.width / 2 - 60, canvas.height / 2);
@@ -152,36 +140,34 @@ function gameLoop ()
 
   if (!gameOver)
   {
-    requestAnimationFrame(gameLoop);
+    window.requestAnimationFrame(gameLoop);
   }
 }
 
-function generateEnemies ()
+function spawnEnemy ()
 {
-  const enemyWidth = 40;
-  const enemyHeight = 60;
-
-  // Adjust the spawn frequency by reducing the interval (currently set to 800 milliseconds)
-  if (Math.random() < 0.5 && enemies.length < 5)
+  if (enemies.length < 2 || enemies.length < 5 && Math.random() < .5)
   {
-    const speedMultiplier = Math.random() < 0.5 ? 1 : -1;
-    const speedVariation = Math.random() * 2 + 1; // Speed variation between 1 and 3
-    const verticalDistance = Math.random() * 200 + 50; // Vertical distance between 50 and 250
+    // Random initial direction.
+    const speedMultiplier = Math.random() < .5 ? 1 : -1;
+
+    // Speed variation between 1 and 3.
+    const speedVariation = Math.random() * 2 + 1;
+
     const enemy = {
-      x: Math.random() * (canvas.width - enemyWidth),
-      y: -enemyHeight - verticalDistance,
-      width: enemyWidth,
-      height: enemyHeight,
+      width: 40,
+      height: 60,
+      x: Math.random() * (canvas.width - 40),
+      y: -60,
+      color: 'red',
       speed: speedMultiplier * speedVariation,
     };
 
-    // Check for vertical distance between the new enemy and existing enemies
-    const safeVerticalDistance = enemies.every(existingEnemy => (
-      enemy.y + enemy.height < existingEnemy.y
-          || existingEnemy.y + existingEnemy.height < enemy.y
-    ));
+    // Check for vertical distance between the new enemy and existing enemies.
+    const safeVerticalDistance = enemies.every(existingEnemy =>
+      enemy.y + enemy.height < existingEnemy.y);
 
-    // If there's sufficient vertical distance, add the new enemy
+    // If there's sufficient vertical distance, add the new enemy.
     if (safeVerticalDistance)
     {
       enemies.push(enemy);
@@ -189,7 +175,8 @@ function generateEnemies ()
   }
 }
 
-// Adjust the interval to control the spawn frequency (currently set to 800 milliseconds)
-setInterval(generateEnemies, 1200);
+// Adjust the interval to control the spawn frequency.
+setInterval(spawnEnemy, 800);
 
-gameLoop();
+// Start the game loop.
+window.requestAnimationFrame(gameLoop);
