80da58ced5041de1617a0a2220a50c8968969313
Bob Vandevliet <info@bvandevliet.nl>
2023-12-29 18:48:48 +0100

    Some improvements regarding oncoming cars.
    * Fixed keydown delay issue.
    * Static sideways speed, reverse on sidewall hit.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 /* eslint-disable func-style */
 /* eslint-disable no-implicit-globals */
 
-// Get the canvas and context
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-// Player car
 const player = {
   x: canvas.width / 2 - 20,
   y: canvas.height - 60,
@@ -13,56 +11,81 @@ const player = {
   height: 60,
   color: 'blue',
   speed: 5,
+  movingLeft: false,
+  movingRight: false,
 };
 
-// Enemy cars
 const enemies = [];
-
-// Game variables
 let score = 0;
 let gameOver = false;
 
-// Function to handle arrow key press
-function handleKeyPress (event)
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
+
+function handleKeyDown (event)
 {
-  if (event.key === 'ArrowLeft' && player.x > 0)
+  if (event.key === 'ArrowLeft')
   {
-    player.x -= player.speed;
+    player.movingLeft = true;
   }
-  else if (event.key === 'ArrowRight' && player.x < canvas.width - player.width)
+  else if (event.key === 'ArrowRight')
   {
-    player.x += player.speed;
+    player.movingRight = true;
   }
 }
 
-// Event listener for arrow key input
-document.addEventListener('keydown', handleKeyPress);
+function handleKeyUp (event)
+{
+  if (event.key === 'ArrowLeft')
+  {
+    player.movingLeft = false;
+  }
+  else if (event.key === 'ArrowRight')
+  {
+    player.movingRight = false;
+  }
+}
 
-// Function to draw the player car
 function drawPlayer ()
 {
   ctx.fillStyle = player.color;
   ctx.fillRect(player.x, player.y, player.width, player.height);
 }
 
-// Function to draw an enemy car
 function drawEnemy (enemy)
 {
   ctx.fillStyle = 'red';
   ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
 }
 
-// Function to move the enemy cars
+function movePlayer ()
+{
+  if (player.movingLeft && player.x > 0)
+  {
+    player.x -= player.speed;
+  }
+  else if (player.movingRight && player.x < canvas.width - player.width)
+  {
+    player.x += player.speed;
+  }
+}
+
 function moveEnemies ()
 {
   for (let i = 0; i < enemies.length; i++)
   {
     enemies[i].y += 2;
-    // Add left-right movement to make it challenging
-    enemies[i].x += Math.sin(enemies[i].y / 50) * 5;
+
+    // Move left and right at a static speed
+    enemies[i].x += enemies[i].speed;
+
+    // Reverse direction when hitting the sidewalls
+    if (enemies[i].x < 0 || enemies[i].x + enemies[i].width > canvas.width)
+    {
+      enemies[i].speed *= -1;
+    }
   }
 
-  // Remove enemies that have moved off the screen
   enemies.forEach((enemy, index) =>
   {
     if (enemy.y > canvas.height)
@@ -73,7 +96,6 @@ function moveEnemies ()
   });
 }
 
-// Function to check for collisions
 function checkCollisions ()
 {
   enemies.forEach(enemy =>
@@ -88,7 +110,6 @@ function checkCollisions ()
       gameOver = true;
     }
 
-    // Check for collision with roadside barrier
     if (player.x < 0 || player.x + player.width > canvas.width)
     {
       gameOver = true;
@@ -96,17 +117,16 @@ function checkCollisions ()
   });
 }
 
-// Function to update the game state
 function update ()
 {
   if (!gameOver)
   {
+    movePlayer();
     moveEnemies();
     checkCollisions();
   }
 }
 
-// Function to draw the game
 function draw ()
 {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -115,19 +135,16 @@ function draw ()
 
   enemies.forEach(drawEnemy);
 
-  // Display score
   ctx.fillStyle = 'black';
   ctx.font = '20px Arial';
   ctx.fillText(`Score: ${score}`, 10, 30);
 
-  // Display game over message
   if (gameOver)
   {
     ctx.fillText('Game Over', canvas.width / 2 - 60, canvas.height / 2);
   }
 }
 
-// Function to run the game loop
 function gameLoop ()
 {
   update();
@@ -139,24 +156,26 @@ function gameLoop ()
   }
 }
 
-// Function to generate random enemy cars
 function generateEnemies ()
 {
   const enemyWidth = 40;
   const enemyHeight = 60;
 
-  const enemy = {
-    x: Math.random() * (canvas.width - enemyWidth),
-    y: -enemyHeight,
-    width: enemyWidth,
-    height: enemyHeight,
-  };
+  // Ensure only one enemy per row
+  if (Math.random() < 0.2 && enemies.length < 1)
+  {
+    const enemy = {
+      x: Math.random() * (canvas.width - enemyWidth),
+      y: -enemyHeight,
+      width: enemyWidth,
+      height: enemyHeight,
+      speed: Math.random() < 0.5 ? 2 : -2, // Random initial direction
+    };
 
-  enemies.push(enemy);
+    enemies.push(enemy);
+  }
 }
 
-// Generate an enemy every 1.5 seconds
 setInterval(generateEnemies, 1500);
 
-// Start the game loop
 gameLoop();
